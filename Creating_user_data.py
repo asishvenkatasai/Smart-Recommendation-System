e53972c398163dd3f6eea88ba339c351a0defa29
asishvenkatasai <57251238+asishvenkatasai@users.noreply.github.com>
2020-10-07 10:53:17 +0530

    Add files via upload

---
+++ Creating_user_data.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 12:37:18 2019
+
+"""
+
+"""
+Intution: Few users were studied to identify their personality types. For the sake of demonstration, 6 personality types have been assumed.
+          
+          These users were then asked five thoughtful questions (pertaining psychology analysis) and their responses were observed.
+          
+          This dataset is then used to train an Artificial Neural Network to find a relationship between the personality types and the responses,
+          so that the personality types of the new users can be predicted based on their responses to those five questions.
+"""
+
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('Amazon_Fine_Food_data.csv') 
+
+dataset_by_user = dataset.groupby('UserId')
+
+list_of_users = list(dataset_by_user.groups.keys())
+user_dataset = pd.DataFrame(list_of_users, columns = ['UserId'])
+
+Resp_to_question1 = np.random.randint(4, size = (len(list_of_users), 1))
+Resp_to_question2 = np.random.randint(4, size = (len(list_of_users), 1))
+Resp_to_question3 = np.random.randint(4, size = (len(list_of_users), 1))
+Resp_to_question4 = np.random.randint(4, size = (len(list_of_users), 1))
+Resp_to_question5 = np.random.randint(4, size = (len(list_of_users), 1))
+
+Responses = np.concatenate((Resp_to_question1, Resp_to_question2, Resp_to_question3, Resp_to_question4, Resp_to_question5), axis = 1)
+Total_resp_score = Responses.sum(axis = 1).reshape(len(Responses), 1) + np.random.uniform(low = -1, high = 1, size=(len(Responses), 1))          #adding some error to total score.
+Total_resp_score = np.around(Total_resp_score)
+Total_resp_score[Total_resp_score < 0] = 0
+Total_resp_score = np.around(Total_resp_score / (16 / 5))
+Responses = np.concatenate((Responses, Total_resp_score), axis = 1)
+
+user_dataset['ResponseQ1'] = Responses[:, 0]
+user_dataset['ResponseQ2'] = Responses[:, 1]
+user_dataset['ResponseQ3'] = Responses[:, 2]
+user_dataset['ResponseQ4'] = Responses[:, 3]
+user_dataset['ResponseQ5'] = Responses[:, 4]
+user_dataset['PersonalityType'] = Responses[:, 5]
+
+user_dataset.to_csv('Amazon_user_data.csv', sep=',', index = False)
---
+++ README.md
@@ -0,0 +1,18 @@
+# Smart-Recommendation-System
+A recommendation system that recommends products to customers by learning their psychology.
+
+Important: Original dataset 'Amazon_Fine_Food_data.csv' was too large to upload, hence not present here.
+
+**Refer 'Logical Data Model.doxc' and 'Solution Design Architecture.docx' for Overview/Summary     of performance and implimenation.
+ 
+1.) Get the dataset and make sure the file name is 'Amazon_Fine_Food_data_2.csv'.
+
+2.) Put the dataset and all the code files in the same directory.
+
+3.) First run 'Creating_user_data.py' to create a sample dataset of users and their presonality types, 'Amazon_user_data.csv'.
+    This data represents already studied users and forms the training/testing data for the following presonality predition model.
+    
+4.) Now run 'User_psychology_prediction_system.py' to train an Artificial Neural Network that predicts users personality type.
+    An accuracy of appox. 85% is achieved.
+    
+5.) Finally, run the 'Smart_recommendation_system.csv' file to get the recommended items for specific personality types, sorted based on product rating.
